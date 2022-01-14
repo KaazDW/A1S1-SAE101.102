9c293572325c646b16234f217ace6b7aa66ac12a
Aylin Saricam <aylinsc83@gmail.com>
2022-01-14 17:01:26 +0100

    Aylin

--- Source.cpp
+++ Source.cpp
@@ -5,5 +5,21 @@ int main() {
 	//tentative de push 
 	//commentaire
 	// Aylin test
+
 	return 0;
 }
+
+
+struct Taille{
+	int Jour;
+	int TailleMax;
+	int TailleMoy;
+	int TailleMin;
+}
+
+struct RobotPanda {
+	int nRobot;
+	int Batterie;
+};
+
+
