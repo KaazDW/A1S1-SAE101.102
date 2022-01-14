9b20d80172c7da37a7c7f0e1b08762d6a989f345
KaazDW <cz2600036@gmail.com>
2022-01-14 15:51:17 +0100

    UPDATE

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,17 @@
+//BRANCHE DE JF
+//  :)
+
 #include <iostream>
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include "config_sdl.h"
 using namespace std;
 
-int main() {
-	//jf branche
+
+
+
+int main(int argc, char* argv[]) {
+
 
 	return 0;
 }
