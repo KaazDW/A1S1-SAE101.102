e925d8c919f8c6e636ee87d8e2aa5cfd8fb5d4c8
Suela Tafa <rakel.tafa@gmail.com>
2022-01-14 15:22:48 +0100

    Test

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include "config_sdl.h"
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
 	//tentative de push 
 	//commentaire
 	// Rakel test
-	//dev
 	return 0;
 }
