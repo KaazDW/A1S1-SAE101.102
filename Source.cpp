1b3d4f94236eed7cad2150594d3a56f2588ea2ae
KaazDW <cz2600036@gmail.com>
2022-01-18 10:58:53 +0100

    UPDATE

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include "config_sdl.h"
+#include<fstream>
 using namespace std;
 
 
@@ -162,6 +163,17 @@ void batterie_et_decoupe(Bambou tab[], Robot& panda1, Robot& panda2, int indice_
 }
 
 
+int somme_croissance(Bambou tab[], int taille) {
+	int somme_croissance_bambou = 0;
+
+	for (int i = 0; i < taille; i++) {
+		somme_croissance_bambou += tab[i].croissance;
+	}
+
+	return somme_croissance_bambou;
+}
+
+
 void ReduceMax(Bambou tab[], int taille, Robot& panda1, Robot& panda2) {
 	int indice_premier = 0, indice_deuxieme = 0;
 	TailleMax(tab, taille, indice_premier, indice_deuxieme);
@@ -174,11 +186,7 @@ void ReduceMax(Bambou tab[], int taille, Robot& panda1, Robot& panda2) {
 
 void ReduceFast(Bambou tab[], int taille, Robot& panda1, Robot& panda2) {
 
-	int somme_croissance_bambou = 0;
-
-	for (int i = 0; i < taille; i++) {
-		somme_croissance_bambou += tab[i].croissance;
-	}
+	int somme_croissance_bambou = somme_croissance(tab, taille);
 
 	double x = 1 + sqrt(5);
 	double taille_minimale = x * somme_croissance_bambou;
@@ -262,7 +270,152 @@ void afficheStats(Statistique tab[], int jour, int indice_premier_plus_grand, in
 }
 
 
-// Fonction en rapport a SDL et constantes
+void Sauvegarde_Jardin_Jour_Robot(Bambou tab[], Robot& panda1, Robot& panda2, int taille, int cpt_jour) {
+	ofstream sortie("jardin.txt", ios::trunc);
+
+	for (int i = 0; i < taille; i++) {
+		sortie << tab[i].num << ',';
+		sortie << tab[i].taille << ',';
+		sortie << tab[i].croissance << endl;
+	}
+
+	for (int j = 0; j < taille; j++) {
+		if (panda1.position[j] == true) {
+			sortie << panda1.batterie << ',';
+			sortie << j << endl;
+		}
+		if (panda2.position[j] == true) {
+			sortie << panda2.batterie << ',';
+			sortie << j << endl;
+		}
+	}
+
+	sortie << cpt_jour << endl;
+
+	sortie.close();
+}
+
+void Recharge_Sauvegarde_Jardin_Jour_Robot(Bambou tab[], Robot& panda1, Robot& panda2, int taille_tab, int& cpt_jour) {
+
+	ifstream entree("jardin.txt", ios::in);//declaration du flot 
+	if (!entree) //si le flot vaut false
+		cout << "Probleme d'ouverture " << endl;
+	else {
+		//lecture du fichier ici (cf. ci-dessous)
+
+		int i = 0;
+		for (; i < taille_tab; i++) {
+			char c_num[100], c_taille[100], c_croissance[100];
+			entree.getline(c_num, 100, ',');
+			entree.getline(c_taille, 100, ',');
+			entree.getline(c_croissance, 100);
+
+			int Num = atoi(c_num);
+			int Taille = atoi(c_taille);
+			int Croissance = atoi(c_croissance);
+
+			tab[i].num = Num;
+			tab[i].taille = Taille;
+			tab[i].croissance = Croissance;
+		}
+
+		char batterie[100], position[100];
+		entree.getline(batterie, 100, ',');
+		entree.getline(position, 100);
+
+		int bat = atoi(batterie);
+		int pos = atoi(position);
+
+		panda1.batterie = bat;
+		panda1.position[pos] = 1;
+
+		entree.getline(batterie, 100, ',');
+		entree.getline(position, 100);
+
+		bat = atoi(batterie);
+		pos = atoi(position);
+
+		panda2.batterie = bat;
+		panda2.position[pos] = 1;
+
+		char jour[100];
+		entree.getline(jour, 100);
+		cpt_jour = atoi(jour);
+
+		entree.close();
+	}
+
+}
+
+void Sauvegarde_Stats_Graphique(Statistique tab[], int taille) {
+	ofstream sortie("stats.txt", ios::trunc);
+
+	for (int i = 0; i < taille; i++) {
+		if (tab[i].Jour >= 0) {
+			sortie << tab[i].Jour << endl;
+			sortie << tab[i].TailleMin << endl;
+			sortie << tab[i].ind_TailleMax1 << ',' << tab[i].val_TailleMax1 << endl;
+			sortie << tab[i].ind_TailleMax2 << ',' << tab[i].val_TailleMax2 << endl;
+			sortie << tab[i].TailleMoy << endl;
+		}
+		else {
+			return;
+		}
+	}
+}
+
+void Recharge_Sauvegarde_Stats_Graphique(Statistique tab[], int taille) {
+	ifstream entree("stats.txt", ios::in);//declaration du flot 
+	if (!entree) //si le flot vaut false
+		cout << "Probleme d'ouverture " << endl;
+	else {
+		//lecture du fichier ici (cf. ci-dessous)
+
+		while (!entree.eof()) {
+
+			char jour[100], min[100], ind_max1[100], val_max1[100], ind_max2[100], val_max2[100], moy[100];
+
+			entree.getline(jour, 100);
+			entree.getline(min, 100);
+			entree.getline(ind_max1, 100, ',');
+			entree.getline(val_max1, 100);
+			entree.getline(ind_max2, 100, ',');
+			entree.getline(val_max2, 100);
+			entree.getline(moy, 100);
+
+			int _jour = atoi(jour);
+			int _min = atoi(min);
+			int _ind_max1 = atoi(ind_max1);
+			int _val_max1 = atoi(val_max1);
+			int _ind_max2 = atoi(ind_max2);
+			int _val_max2 = atoi(val_max2);
+			float _moy = atof(moy);
+
+			tab[_jour].Jour = _jour;
+			tab[_jour].TailleMin = _min;
+			tab[_jour].ind_TailleMax1 = _ind_max1;
+			tab[_jour].val_TailleMax1 = _val_max1;
+			tab[_jour].ind_TailleMax2 = _ind_max2;
+			tab[_jour].val_TailleMax2 = _val_max2;
+			tab[_jour].TailleMoy = _moy;
+		}
+
+		entree.close();
+	}
+}
+
+
+/*
+
+	 __  __  ___   ____   __           __  __
+	/ / / / / __| | __ \ | |          / / / /
+   / / / / | (___ | |  | | |         / / / /
+  / / / /   \___ \| |  | | |        / / / /
+ / / / /    ____) | |__| | |____   / / / /
+/_/ /_/    |_____/|_____/|______| /_/ /_/
+
+
+*/
 
 const int PositionX_leaveButton = 700;
 const int PositionY_leaveButton = 500;
@@ -271,21 +424,27 @@ const int HauteurFenetre = 880;
 SDL_Rect returnmenu_button;
 SDL_Rect rectborduregauche;
 SDL_Rect rectborduredroite;
+SDL_Rect RectChoixSaveGauche;
+SDL_Rect RectChoixSaveMillieu;
+SDL_Rect RectChoixSaveDroite;
 
+bool ActivChoixDroite = false;
+bool ActivChoixMillieu = false;
+bool ActivChoixGauche = false;
 bool ActivStartMenu = false;
+
 
 void affiche_terre_bambou(SDL_Renderer* rendu) {
 	SDL_SetRenderDrawColor(rendu, 255, 215, 0, 255);
-	SDL_RenderDrawLine(rendu, 70, 820, 850, 820);
+	SDL_RenderDrawLine(rendu, 70, 720, 850, 720);
 	SDL_RenderPresent(rendu);
-
-
 }
+
 
 void affiche(SDL_Renderer* rendu) {
 	ActivStartMenu = false;
 	SDL_RenderClear(rendu);
-	SDL_Rect rectarriereplan;
+	SDL_Rect rectarriereplan; //fond noir
 	rectarriereplan.w = LargeurFenetre;
 	rectarriereplan.h = HauteurFenetre;
 	rectarriereplan.y = 0;
@@ -293,7 +452,7 @@ void affiche(SDL_Renderer* rendu) {
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 0);
 	SDL_RenderFillRect(rendu, &rectarriereplan);
 
-	SDL_Rect rectfond;
+	SDL_Rect rectfond; //fond gauche affichage des bambou
 	rectfond.w = LargeurFenetre - 750;
 	rectfond.h = HauteurFenetre - 50;
 	rectfond.y = 25;
@@ -301,7 +460,7 @@ void affiche(SDL_Renderer* rendu) {
 	SDL_SetRenderDrawColor(rendu, 0, 30, 40, 255);
 	SDL_RenderFillRect(rendu, &rectfond);
 
-	SDL_Rect rect;
+	SDL_Rect rect; //fond millieu affichage des graphiques
 	rect.w = LargeurFenetre - 1245;
 	rect.h = HauteurFenetre - 50;
 	rect.y = 25;
@@ -309,19 +468,41 @@ void affiche(SDL_Renderer* rendu) {
 	SDL_SetRenderDrawColor(rendu, 0, 30, 40, 255);
 	SDL_RenderFillRect(rendu, &rect);
 
-	rect.w = 280;
+	rect.w = 280; //fond droite affichage des info stat button
 	rect.h = HauteurFenetre - 50;
 	rect.y = 25;
 	rect.x = LargeurFenetre - 300;
 	SDL_SetRenderDrawColor(rendu, 0, 30, 40, 255);
 	SDL_RenderFillRect(rendu, &rect);
 
-	returnmenu_button.w = 250;
+	returnmenu_button.w = 250; //Button retour au menu de choix
 	returnmenu_button.h = 50;
 	returnmenu_button.y = HauteurFenetre - 90;
 	returnmenu_button.x = LargeurFenetre - 285;
 	SDL_SetRenderDrawColor(rendu, 0, 15, 15, 255);
 	SDL_RenderFillRect(rendu, &returnmenu_button);
+
+	/*Button de choix save*/
+	int TailleCarreButton = 70;
+	RectChoixSaveGauche.w = TailleCarreButton;//Gauche
+	RectChoixSaveGauche.h = TailleCarreButton;
+	RectChoixSaveGauche.y = HauteurFenetre - 180;
+	RectChoixSaveGauche.x = LargeurFenetre - 285;
+	SDL_SetRenderDrawColor(rendu, 50, 0, 15, 255);
+	SDL_RenderFillRect(rendu, &RectChoixSaveGauche);
+	RectChoixSaveMillieu.w = TailleCarreButton;//Millieu
+	RectChoixSaveMillieu.h = TailleCarreButton;
+	RectChoixSaveMillieu.y = HauteurFenetre - 180;
+	RectChoixSaveMillieu.x = LargeurFenetre - 285 + 90;
+	SDL_SetRenderDrawColor(rendu, 50, 0, 15, 255);
+	SDL_RenderFillRect(rendu, &RectChoixSaveMillieu);
+	RectChoixSaveDroite.w = TailleCarreButton;//Droite
+	RectChoixSaveDroite.h = TailleCarreButton;
+	RectChoixSaveDroite.y = HauteurFenetre - 180;
+	RectChoixSaveDroite.x = LargeurFenetre - 285 + 90 * 2;
+	SDL_SetRenderDrawColor(rendu, 50, 0, 15, 255);
+	SDL_RenderFillRect(rendu, &RectChoixSaveDroite);
+
 
 
 	affiche_terre_bambou(rendu);
@@ -405,6 +586,34 @@ int start_choice(SDL_Renderer* rendu) { /*Menu de choix*/
 }
 
 
+void bambous_tracer_pour_reducemax(SDL_Renderer* rendu, Bambou jardin[], int taille_jardin) {
+
+	SDL_Rect rectangle_inferieur;
+	SDL_Rect rectangle_superieur;
+
+	rectangle_inferieur.w = 8;
+	rectangle_inferieur.h = 4;
+
+	rectangle_superieur.w = 12;
+	rectangle_superieur.h = 1;
+
+	for (int i = 0; i < taille_jardin; i++) {
+		rectangle_inferieur.x = 62 * i + 95;
+		rectangle_superieur.x = rectangle_inferieur.x - 2;
+
+		for (int j = 0; j < jardin[i].taille; j++) {
+			rectangle_superieur.y = 715 - 5 * j;
+			SDL_SetRenderDrawColor(rendu, 69, 224, 11, 255); // couleur verte
+			SDL_RenderFillRect(rendu, &rectangle_superieur);
+			rectangle_inferieur.y = rectangle_superieur.y + 1;
+			SDL_SetRenderDrawColor(rendu, 151, 160, 9, 255); // couleur verte plus fonce
+			SDL_RenderFillRect(rendu, &rectangle_inferieur);
+		}
+	}
+	SDL_RenderPresent(rendu);
+}
+
+
 int main(int argc, char* argv[]) {
 
 	// Declaration tableau et constante
@@ -421,10 +630,25 @@ int main(int argc, char* argv[]) {
 	int indice_premier_plus_grand = 0, indice_deuxieme_plus_grand = 0;
 
 	Robot panda1, panda2;
-	InitRobot(panda1);
-	InitRobot(panda2);
 
-	/*
+	ifstream entree("jardin.txt", ios::in);
+	ifstream entree2("stats.txt", ios::in);
+	if (!entree) {
+		if (!entree2) {
+			InitTab(jardin, TAILLE);
+			InitRobot(panda1);
+			InitRobot(panda2);
+		}
+		else {
+			cout << "Erreur, il manque un fichier de sauvegarde." << endl;
+		}
+	}
+	else {
+		Recharge_Sauvegarde_Stats_Graphique(RecupStats, TAILLE_STATS);
+		Recharge_Sauvegarde_Jardin_Jour_Robot(jardin, panda1, panda2, TAILLE, cpt_jour);
+	}
+
+
 	bool simulation = true;
 	char choix_suite, mode;
 
@@ -441,6 +665,9 @@ int main(int argc, char* argv[]) {
 			if (choix_suite == 'q') {
 				cout << "Fin." << endl;
 				simulation = false;
+
+				Sauvegarde_Stats_Graphique(RecupStats, TAILLE_STATS);
+				Sauvegarde_Jardin_Jour_Robot(jardin, panda1, panda2, TAILLE, cpt_jour);
 			}
 
 			else if (choix_suite == 'r') {
@@ -461,6 +688,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	/*
 	else if (mode == 'f') {
 
 		while (simulation) {
@@ -531,14 +759,6 @@ int main(int argc, char* argv[]) {
 						//on peut enlever SDL_Delay
 			continuer = false;
 			break;
-		case SDL_KEYDOWN:
-			if (event.key.keysym.sym == SDLK_r) { //touche r
-				if (ActivStartMenu == false && ) {
-		
-
-				}
-			}
-			break;
 		case SDL_MOUSEBUTTONUP://appui souris
 			if (event.button.button == SDL_BUTTON_LEFT) {
 				if (ActivStartMenu == true &&
@@ -546,43 +766,77 @@ int main(int argc, char* argv[]) {
 					event.button.x<rectborduredroite.x + rectborduredroite.w &&
 					event.button.y>rectborduredroite.y &&
 					event.button.y < rectborduredroite.y + rectborduredroite.h) {
+					ActivChoixGauche = false;
+					ActivStartMenu = false;
+					ActivChoixDroite = true;
 					affiche(rendu);
 				}
 				SDL_RenderPresent(rendu);//on rafraichit
-				if (event.button.button == SDL_BUTTON_LEFT) {
-					if (ActivStartMenu == true &&
-						event.button.x > rectborduregauche.x &&
-						event.button.x<rectborduregauche.x + rectborduregauche.w &&
-						event.button.y>rectborduregauche.y &&
-						event.button.y < rectborduregauche.y + rectborduregauche.h) {
-						affiche(rendu);
-					}
+				if (ActivStartMenu == true &&
+					event.button.x > rectborduregauche.x &&
+					event.button.x<rectborduregauche.x + rectborduregauche.w &&
+					event.button.y>rectborduregauche.y &&
+					event.button.y < rectborduregauche.y + rectborduregauche.h) {
+					ActivChoixGauche = true;
+					ActivStartMenu = false;
+					ActivChoixDroite = true;
+					affiche(rendu);
+					bambous_tracer_pour_reducemax(rendu, jardin, TAILLE);
+				}
 				SDL_RenderPresent(rendu);//on rafraichit
-				if (event.button.button == SDL_BUTTON_LEFT) {//si on clique bouton gauche
-					if (event.button.x > returnmenu_button.x &&
-						event.button.x<returnmenu_button.x + returnmenu_button.w &&
-						event.button.y>returnmenu_button.y &&
-						event.button.y < returnmenu_button.y + returnmenu_button.h) {
-						start_choice(rendu);
-					}
-
-						SDL_RenderPresent(rendu);//on rafraichit
-						break;
-					}
+				if (event.button.x > returnmenu_button.x &&
+					event.button.x<returnmenu_button.x + returnmenu_button.w &&
+					event.button.y>returnmenu_button.y &&
+					event.button.y < returnmenu_button.y + returnmenu_button.h) {
+					start_choice(rendu);
+				}
+				SDL_RenderPresent(rendu);//on rafraichit
+				if (ActivChoixGauche == true &&
+					event.button.x > RectChoixSaveGauche.x &&
+					event.button.x<RectChoixSaveGauche.x + RectChoixSaveGauche.w &&
+					event.button.y>RectChoixSaveGauche.y &&
+					event.button.y < RectChoixSaveGauche.y + RectChoixSaveGauche.h) {
+					//fonction pour button Gauche
 
 				}
+				SDL_RenderPresent(rendu);//on rafraichit
+				if (ActivChoixGauche == true &&
+					event.button.x > RectChoixSaveMillieu.x &&
+					event.button.x<RectChoixSaveMillieu.x + RectChoixSaveMillieu.w &&
+					event.button.y>RectChoixSaveMillieu.y &&
+					event.button.y < RectChoixSaveMillieu.y + RectChoixSaveMillieu.h) {
+					//fonction pour button Millieu
+
+				}
+				SDL_RenderPresent(rendu);//on rafraichit
+				if (ActivChoixGauche == true &&
+					event.button.x > RectChoixSaveDroite.x &&
+					event.button.x<RectChoixSaveDroite.x + RectChoixSaveDroite.w &&
+					event.button.y>RectChoixSaveDroite.y &&
+					event.button.y < RectChoixSaveDroite.y + RectChoixSaveDroite.h) {
+					//fonction pour button Droite
+				}
+				SDL_RenderPresent(rendu);//on rafraichit
+
+
+
+
+
+				break;
+
 			}
-			//destruction du renderer à la fin
-			SDL_DestroyRenderer(rendu);
 
-			//destruction à la fin
-			SDL_DestroyWindow(win);   //equivalent du delete
-
-			//fermeture
-			SDL_Quit();
 		}
-
 	}
+	//destruction du renderer à la fin
+	SDL_DestroyRenderer(rendu);
+
+	//destruction à la fin
+	SDL_DestroyWindow(win);   //equivalent du delete
+
+	//fermeture
+	SDL_Quit();
+
 
 	return 0;
 }
